653a4f8ea3b1a0d28d60a49c3ea144a341c031c4
PerThoGon <enzo.marini888@gmail.com>
2023-03-13 23:20:40 +0100

    SignOut + debut fav

--- Animania/components/Profil.jsx
+++ Animania/components/Profil.jsx
@@ -1,15 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { auth } from '../firebase';
 import { useNavigation } from "@react-navigation/native";
 
 export default function Profil() {
+    const navigation = useNavigation();
+
+    const handleSIgnOut = () => {
+        auth
+          .signOut()
+          .then(() => {
+            navigation.reset({
+              index: 0,
+              routes: [{ name: "LoginScreen" }],
+            });
+          })
+          .catch((error) => alert(error.message));
+    };
   return (
     <View style={styles.container}>      
         <StatusBar style="auto" />
         <View style={styles.titleContainer}>
             <Text style={styles.title}>Animania</Text>
+        </View>
+        <View style={styles.EmailContainer}>
+            <Text style={styles.EmailTitre}>Actuellement connecté avec l'email :</Text>
+            <Text style={styles.Email}>{auth.currentUser?.email}</Text>
+        </View>
+        <View style={styles.SignOut}>
+            <TouchableOpacity style={styles.SignOutButton} onPress={handleSIgnOut}>
+                <Text style={styles.SignOutTitle}>
+                    Sign Out
+                </Text>
+            </TouchableOpacity>
         </View>
     </View>
   )
@@ -32,5 +56,34 @@ const styles = StyleSheet.create({
     fontSize: 40,
     color: 'blue',
     fontWeight: 'bold',
+  },
+  EmailContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: '40%',
+  },
+  EmailTitre: {
+    fontSize: 15,
+  },
+  Email: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  SignOut:{
+    alignItems: "center",
+    justifyContent: 'center',
+  },
+  SignOutButton:{
+    backgroundColor: "blue",
+    marginTop: '40%',
+    width: "60%",
+    padding: 15,
+    borderRadius: 10,
+  },
+  SignOutTitle: {
+    color: "white",
+    fontWeight: 700,
+    fontSize: 16,
+    textAlign: 'center',
   },
 });
