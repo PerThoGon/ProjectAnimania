0bcae022abce575f1d61b5526ee06faa05b09982
PerThoGon <enzo.marini888@gmail.com>
2023-03-13 21:31:47 +0100

    loginScreen

--- Animania/App.jsx
+++ Animania/App.jsx
@@ -1,19 +1,60 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer, PrivateValueStore } from '@react-navigation/native';
+import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Accueil from './components/Accueil';
 import Favoris from './components/Favoris';
 import Profil from './components/Profil';
 import Recherche from './components/Recherche';
+import LoginScreen from './components/LogInScreen';
 
 const tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+export default function App(){
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LoginScreen" >
+        <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ headerShown: false }}/>
+        <Stack.Screen name="TabBar" component={TabBar} options={{ headerShown: false }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+function TabBar() {
+  return (    
+    <tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+          if (route. name == "Accueil") {
+            iconName = "home-outline"
+          } else if (route. name == "Recherche") {
+            iconName = "search-outline"
+          } else if (route. name == "Favoris") {
+            iconName = "star-outline"
+          } else if (route. name == "Profil") {
+            iconName = "person-outline"
+          }
+          return <Ionicons name={iconName} size={20} color='#000'/>
+        }
+      })} 
+    >
+      <tab.Screen name='Accueil' component={AccueilScreen} options={{ headerShown: false }}/>
+      <tab.Screen name='Recherche' component={RechercheScreen} options={{ headerShown: false }}/>
+      <tab.Screen name='Favoris' component={FavorisScreen} options={{ headerShown: false }}/>
+      <tab.Screen name='Profil' component={ProfilScreen} options={{ headerShown: false }}/>
+    </tab.Navigator>
+  );
+}
 
 function AccueilScreen() {
   return (
     <View style={styles.container}>
       <Accueil/>
-    </View>
+    </View>    
   );
 }
 
@@ -37,45 +78,10 @@ function ProfilScreen() {
   return (
     <View style={styles.container}>
       <Profil />
-    </View>
+    </View>    
   );
-}
+}  
 
-function TabBar() {
-  return (
-    <NavigationContainer>
-      <tab.Navigator
-        screenOptions={({route}) => ({
-          tabBarIcon: ({focused, color, size}) => {
-            let iconName;
-            if (route. name == "Accueil") {
-              iconName = "home-outline"
-            } else if (route. name == "Recherche") {
-              iconName = "search-outline"
-            } else if (route. name == "Favoris") {
-              iconName = "star-outline"
-            } else if (route. name == "Profil") {
-              iconName = "person-outline"
-            }
-            return <Ionicons name={iconName} size={20} color='#000'/>
-          }
-        })} 
-      >
-        <tab.Screen name='Accueil' component={AccueilScreen} options={{ headerShown: false }}/>
-        <tab.Screen name='Recherche' component={RechercheScreen} options={{ headerShown: false }}/>
-        <tab.Screen name='Favoris' component={FavorisScreen} options={{ headerShown: false }}/>
-        <tab.Screen name='Profil' component={ProfilScreen} options={{ headerShown: false }}/>
-      </tab.Navigator>
-    </NavigationContainer>
-  );
-}
-
-export default function App(){
-  return
-    <View>
-      <Profil/>
-    </View>
-}
 
 const styles = StyleSheet.create({
   container: {
--- Animania/components/Profil.jsx
+++ Animania/components/Profil.jsx
@@ -1,138 +1,14 @@
-import { StyleSheet, Text, TextInput, TouchableOpacity, View, } from "react-native";
-import React, { useState, useEffect } from "react";
-import { useNavigation } from "@react-navigation/native";
-import {auth} from "../firebase";
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-export default function Profil() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-
-    const navigation = useNavigation();
-
-    useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged((user) => {
-        if (user) {
-            navigation.replace("Home");
-        }
-        });
-        return unsubscribe;
-    }, []);
-
-    const SignUp = () => {
-        auth
-          .createUserWithEmailAndPassword(email, password)
-          .then((userCredentials) => {
-            const user = userCredentials.user;
-            console.log("Registered in with:", user.email);
-          })
-          .catch((error) => alert(error.message));
-      };
-    
-      const SignIn = () => {
-        auth
-          .signInWithEmailAndPassword(email, password)
-          .then((userCredentials) => {
-            const user = userCredentials.user;
-            console.log("Logged in with:", user.email);
-          })
-          .catch((error) => alert(error.message));
-      };
-
+const Profil = () => {
   return (
-    <View style={styles.container} behavior="padding">
-    <View style={styles.titleContainer}>
-        <Text style={styles.title}>Animania</Text>
+    <View>
+      <Text>Profil</Text>
     </View>
-    <View style={styles.inputContainer}>
-        <TextInput
-        placeholder="Email"
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-        style={styles.input}
-        />
-        <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-        style={styles.input}
-        secureTextEntry
-        />
-    </View>
-    <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={SignIn} style={styles.button}>
-        <Text style={styles.buttonText}>Login</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-        onPress={SignUp}
-        style={[styles.button, styles.buttonOutline]}
-        >
-        <Text style={styles.buttonOutlineText}>Register</Text>
-        </TouchableOpacity>
-    </View>
-    </View>
-  );
-};
+  )
+}
 
-const styles = StyleSheet.create({
-container: {
-    backgroundColor:'#FFF',
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-},
-titleContainer: {
-  flexDirection: 'row',
-  alignItems: 'center',
-  justifyContent: 'center',
-  paddingHorizontal: 10,
-},
-title: {
-  fontSize: 40,
-  color: 'blue',
-  fontWeight: 'bold',
-},
-inputContainer: {
-    width: "80%",
-},
-input: {
-    borderColor: "blue",
-    backgroundColor: "white",
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-    borderRadius: 10,
-    marginTop: 10,
-},
-buttonContainer: {
-    width: "60%",
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 40,
-},
-button: {
-    backgroundColor: "blue",
-    width: "100%",
-    padding: 15,
-    borderRadius: 10,
-    alignItems: "center",
-},
-buttonOutline: {
-    backgroundColor: "white",
-    marginTop: 10,
-    borderColor: "blue",
-    borderWidth: 2,
-},
-buttonText: {
-    color: "white",
-    fontWeight: 700,
-    fontSize: 16,
-},
-buttonOutlineText: {
-    color: "blue",
-    fontWeight: 700,
-    fontSize: 16,
-},
-});
+export default Profil
 
-
-
-
+const styles = StyleSheet.create({})
