b7250b13407e9d977bebbe7c9cb09c153d23dad6
PerThoGon <enzo.marini888@gmail.com>
2023-03-14 15:44:18 +0100

    vueAnimeRecherche

--- Animania/components/Recherche.jsx
+++ Animania/components/Recherche.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity, FlatList, Image } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, FlatList, Image, Modal, ScrollView, } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import axios from 'axios';
 import { Linking } from 'react-native';
@@ -8,6 +8,8 @@ import { StatusBar } from 'expo-status-bar';
 export default function Recherche() {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [isVueAnimeModalVisible, setisVueAnimeModalVisible] = useState(false);
+  const [selectedAnime, setSelectedAnime] = useState(null);
 
   async function searchAnime() {
     try {
@@ -16,6 +18,16 @@ export default function Recherche() {
     } catch (error) {
       console.error(error);
     }
+  }
+
+  function vueAnime(item) {
+    setSelectedAnime(item);
+    setisVueAnimeModalVisible(true);
+  }
+
+  function closeModal() {
+    setisVueAnimeModalVisible(false);
+    setSelectedAnime(null);
   }
 
   return (
@@ -41,19 +53,45 @@ export default function Recherche() {
           keyExtractor={(item) => item.mal_id.toString()}
           renderItem={({ item }) => (
             <View style={styles.topAnimeItem}>
-              <Image source={{ uri: item.images.jpg.image_url }} style={styles.AnimeItemImage} />
-              <Text style={styles.AnimeItemTitle}>{item.title})</Text>
-              <View style={styles.buttonContainer}>
-                <TouchableOpacity style={styles.button} onPress={() => Linking.openURL(item.url)}>
-                  <Text style={styles.buttonText}>Voir sur MyAnimeList</Text>
-                </TouchableOpacity>
-              </View>
+              <TouchableOpacity onPress={() => vueAnime(item)}>
+                <Image source={{ uri: item.images.jpg.image_url }} style={styles.AnimeItemImage} />
+                <Text style={styles.AnimeItemTitle}>{item.title}</Text>
+                <View style={styles.buttonContainer}>
+                  <TouchableOpacity style={styles.button} onPress={() => Linking.openURL(item.url)}>
+                    <Text style={styles.buttonText}>Voir sur MyAnimeList</Text>
+                  </TouchableOpacity>
+                </View>
+              </TouchableOpacity>
             </View>
           )}
           showsHorizontalScrollIndicator={false}
           numColumns={2}  // Ajout de la prop "numColumns"
         />
-      </View>       
+      </View>
+      {selectedAnime && (
+        <View>
+          <Modal visible={isVueAnimeModalVisible} onRequestClose={closeModal}>
+            <StatusBar style="auto" />
+            <View style={styles.titleContainer}>
+              <Text style={styles.title}>Animania</Text>
+            </View>
+            <View style={styles.modalContainer}>
+              <Image source={{ uri: selectedAnime.images.jpg.image_url }} style={styles.modalImage} />
+              <Text style={styles.modalTitle}>{selectedAnime.title}</Text>
+              <ScrollView style={styles.Scroll}>
+                <Text style={styles.modalText}>{selectedAnime.synopsis}</Text>
+                <Text style={styles.modalText}>Score: {selectedAnime.score}</Text>
+                <TouchableOpacity style={styles.modalButtonTexte} onPress={() => Linking.openURL(selectedAnime.url)}>
+                  <Text style={styles.buttonText}>Voir sur MyAnimeList</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.modalButton} onPress={closeModal}>
+                  <Text style={styles.modalButtonText}>Retour</Text>
+                </TouchableOpacity>
+              </ScrollView>
+            </View>
+          </Modal>
+        </View>
+      )}       
     </View>
   );
 }
@@ -134,5 +172,42 @@ const styles = StyleSheet.create({
   topAnimeEpisodes: {
     fontSize: 14,
     color: 'gray',
+  },
+  modalContainer:{
+    alignItems: 'center',
+    padding: 20,
+  },
+  modalImage:{
+    width: '60%',
+    height: 330,
+  },
+  modalTitle:{
+    marginTop: '5%',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  modalText: {
+    marginTop: '5%',
+  },
+  Scroll: {
+    alignContent: 'center',
+    height: 330,
+  },
+  modalButtonTexte: {
+    width: "100%",
+    marginTop: '5%',
+  },
+  modalButton:{
+    marginTop: '5%',
+    backgroundColor: "blue",
+    width: "100%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  modalButtonText: {
+    color: "white",
+    fontWeight: 700,
+    fontSize: 16,
   },
 });
